841bc81cdb692446fb975f38209f153bb3d027ed
ZaidSA <zaid.pathan@solutionanalysts.com>
2019-04-28 17:34:20 +0530

    Initial commit

---
+++ MyCarthageLib/MyCarthageLib/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCarthageLib
+//
+//  Created by Zaid Pathan on 28/04/19.
+//  Copyright © 2019 Zaid Pathan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
