1fa8f959e914d58383c966836a99b3f791cb6839
ZaidSA <zaid.pathan@solutionanalysts.com>
2019-05-01 10:21:22 +0530

    test

---
+++ MyCarthageLibProject/MyCarthageLib/MyCarthageLib.swift
@@ -0,0 +1,17 @@
+//
+//  MyCarthageLib.swift
+//  MyCarthageLib
+//
+//  Created by Zaid Pathan on 01/05/19.
+//  Copyright © 2019 Zaid Pathan. All rights reserved.
+//
+
+import Foundation
+
+public class MyCarthageLib {
+    public static let shared = MyCarthageLib()
+    
+    public func test() {
+        print("It worked!")
+    }
+}
