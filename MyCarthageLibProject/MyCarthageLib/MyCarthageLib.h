bee1a541b0aacccbd2a9a67c2a35a02998f9cece
ZaidSA <zaid.pathan@solutionanalysts.com>
2019-05-01 10:17:32 +0530

    test

---
+++ MyCarthageLibProject/MyCarthageLib/MyCarthageLib.h
@@ -0,0 +1,19 @@
+//
+//  MyCarthageLib.h
+//  MyCarthageLib
+//
+//  Created by Zaid Pathan on 01/05/19.
+//  Copyright © 2019 Zaid Pathan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MyCarthageLib.
+FOUNDATION_EXPORT double MyCarthageLibVersionNumber;
+
+//! Project version string for MyCarthageLib.
+FOUNDATION_EXPORT const unsigned char MyCarthageLibVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MyCarthageLib/PublicHeader.h>
+
+
---
+++ MyCarthageLibProject/MyCarthageLibProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyCarthageLibProject
+//
+//  Created by Zaid Pathan on 01/05/19.
+//  Copyright © 2019 Zaid Pathan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
